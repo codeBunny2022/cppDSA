b2a1ff6b7abe8b0f47d75d7204f17bc2b1cdbc2c
Chirag <chiragcrypt@gmail.com>
2025-08-08 13:28:23 +0530

    pyramid pattern

---
+++ patterns/pyramid.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void pyramid(int n){
+    for(int i=1;i<n;i++){
+        // sapce
+        for(int j=0;j<n-i;j++){
+            cout<<" ";
+        }
+
+        // stars
+        for(int j=0;j<2*i-1;j++){
+            cout<<"*";
+        }
+
+        //space
+        for(int j=0;j< n-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+int main() {
+    int val=0;
+    cout<<"enter the size of pyramid"<<endl;
+    cin>>val;
+    val=val+1;
+    pyramid(val);
+    
+    return 0;
+}
